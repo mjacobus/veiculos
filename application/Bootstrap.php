8ea49827db991f54aa81c01a3526593d2f6d627a
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2010-12-11 11:48:13 -0200

    Adicionado caminho para view helpers

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -81,6 +81,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         ));
         /*TODO: bring to the application.ini*/
         $view->addHelperPath("ZendX/JQuery/View/Helper", "ZendX_JQuery_View_Helper");
+        $view->addHelperPath("App/View/Helper", "App_View_Helper");
         $viewRenderer = new Zend_Controller_Action_Helper_ViewRenderer();
         $viewRenderer->setView($view);
             Zend_Controller_Action_HelperBroker::addHelper($viewRenderer);
