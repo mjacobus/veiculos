968afd10cf3f721ce34ffcf49710e08bf4dba59b
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2010-12-22 18:04:08 -0200

    Adicionado o search

---
+++ application/modules/admin/forms/Search.php
@@ -0,0 +1,24 @@
+<?php
+
+class Admin_Form_Search extends Zend_Form
+{
+
+    public function init()
+    {
+        $search = new Zend_Form_Element_Text('search');
+        $search->setValue(Zend_Controller_Front::getInstance()->getRequest()->getParam('search'));
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel('Pesquisar');
+
+        $this->addElements(array($search, $submit))
+            ->setElementDecorators(array('ViewHelper'))
+            ->setAttrib('id', 'searchForm')
+            ->addDecorator('FormElements')
+            ->addDecorator('HtmlTag', array('tag' => 'div', 'class' => 'search'))
+            ->addDecorator('Form');
+        ;
+    }
+
+}
+
--- application/modules/admin/models/Veiculo.php
+++ application/modules/admin/models/Veiculo.php
@@ -5,7 +5,6 @@ class Admin_Model_Veiculo extends App_Model_Crud
 
     protected $_tableName = 'Veiculo';
 
-    
     public function init()
     {
         $this->_form = new Admin_Form_Veiculo($this);
@@ -20,7 +19,7 @@ class Admin_Model_Veiculo extends App_Model_Crud
         $this->_form->marca_id->addMultiOptionFromDql(
             $dql, 'id', 'nome', array(null => 'Selecione')
         );
-        
+
         $dql = Doctrine_Query::create()->from('Combustivel')->orderBy('nome');
         $this->_form->combustivel_id->addMultiOptionFromDql(
             $dql, 'id', 'nome', array(null => 'Selecione'));
@@ -28,6 +27,42 @@ class Admin_Model_Veiculo extends App_Model_Crud
         $dql = Doctrine_Query::create()->from('VeiculoSituacao')->orderBy('nome');
         $this->_form->situacao_id->addMultiOptionFromDql(
             $dql, 'id', 'nome', array(null => 'Selecione'));
+    }
+
+    /**
+     *
+     * @param array $params
+     * @return Doctrine_Query
+     */
+    public function getQuery(array $params = array())
+    {
+        $dql = parent::getQuery()
+                ->innerJoin('base.Marca M')
+                ->innerJoin('base.Situacao S')
+                ->innerJoin('base.Tipo T')
+                ->innerJoin('base.Combustivel C');
+
+        if (isset($params['search'])) {
+            $fields = array(
+                'base.modelo',
+                'base.cor',
+                'base.placa',
+                'C.nome',
+                'S.nome',
+                'T.nome',
+                'M.nome',
+            );
+
+            $search = preg_replace('/\s/', ' ', $params['search']);
+            $concat = 'CONCAT(' . implode(',', $fields) . ') like ?';
+            $words = explode(' ', $search);
+
+            foreach ($words as $word) {
+                $dql->addWhere($concat, "%$word%");
+            }
+        }
+        
+        return $dql;
     }
 
 }
