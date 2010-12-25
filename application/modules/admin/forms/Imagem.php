2425fda6d5ea5b302d8e303769028add85355406
Marcelo G. Jacobus Jr <marcelo.jacobus@gmail.com>
2010-12-25 00:08:52 -0200

    Melhorado o formulário de imagem

--- application/modules/admin/forms/Imagem.php
+++ application/modules/admin/forms/Imagem.php
@@ -6,7 +6,10 @@ class Admin_Form_Imagem extends App_Form_Abstract
     public function init()
     {
         $this->addDescricao();
+        $this->addArquivo();
+        $this->addImagem();
         $this->addSubmit();
+        $this->getElement('submit')->setDecorators($this->_elementDecorators);
     }
 
     /**
@@ -16,23 +19,41 @@ class Admin_Form_Imagem extends App_Form_Abstract
      */
     public function addDescricao()
     {
-        $this->addElement($this->getTextElement('descricao', 'Descrição'));
+        $element = $this->getTextElement('descricao', 'Descrição')
+            ->setDecorators($this->_elementDecorators);
+        $this->addElement($element);
         return $this;
+    }
+
+    /**
+     * 
+     */
+    public function setElementDecorators()
+    {
+        
     }
 
     /**
      * Add file wich is a Zend_Form_Element_File
      * @return Admin_Form_ImageUpload
      */
-    public function addFile(array $params = array())
+    public function addArquivo(array $params = array())
     {
         $element = new Zend_Form_Element_File('file');
         $this->setRequired($element);
         $element->setLabel('Arquivo')
             ->setDestination(APPLICATION_PATH . '/../tmp/uploads')
             ->addValidator('Extension', false, 'jpg,png,gif,jpeg')
-            ->addValidator('FilesSize', false, array('max' => '5MB'))
-            ->setMaxFileSize(5*1024*1024);
+            ->addValidator('FilesSize', false, array('max' => '2MB'))
+            ->setMaxFileSize(2 * 1024 * 1024)
+            ->setDecorators(array(
+                'Description',
+                'File',
+                'Errors',
+                array(array('element' => 'HtmlTag'), array('tag' => 'div', 'class' => 'element')),
+                array('Label', array('tag' => 'div')),
+                array('HtmlTag', array('tag' => 'div','class' => 'label-and-element'))
+            ));
 
         if (array_key_exists('id', $params)) {
             $element->setDescription("Para alterar imagem prencha este campo.");
@@ -48,12 +69,13 @@ class Admin_Form_Imagem extends App_Form_Abstract
      * Add image wich is a Zend_Form_Element_File
      * @return Admin_Form_ImageUpload
      */
-    public function addImage(array $params = array())
+    public function addImagem(array $params = array())
     {
         if (array_key_exists('id', $params)) {
             $element = new Zend_Form_Element_Image('image', array());
             $element->setLabel('Imagem')
-                ->setAttrib('style', 'width:400px;');
+                ->setAttrib('style', 'width:400px;')
+                ->setDecorators($this->_elementDecorators);;
             $element->getDecorator('HtmlTag')->setOption('style', 'height:310px;');
             $this->addElement($element);
         }
